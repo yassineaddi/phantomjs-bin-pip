58c9f0c1fc6a13a0d05962c16f47f4cd353d5545
Yassine Addi <yassineaddi98@gmail.com>
2020-02-17 10:15:58 +0100

    syntax error

--- phantomjs_bin/__init__.py
+++ phantomjs_bin/__init__.py
@@ -31,7 +31,7 @@ def _get_executable_path():
     setup_path = os.path.dirname(os.path.abspath(__file__))
     bin_path = os.path.join(setup_path, 'bin', os_info["dir"], bin_filename)
     
-    os_type != 'Windows':
+    if os_type != 'Windows':
         st = os.stat(bin_path)
         os.chmod(bin_path, st.st_mode | stat.S_IEXEC)
 
