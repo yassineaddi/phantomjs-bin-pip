b8a1e67e4aab5b045773fa740a406f694bfb2200
Yassine Addi <yassineaddi98@gmail.com>
2020-02-17 10:03:44 +0100

    Update __init__.py
    'phantomjs' executable may have wrong permissions

--- phantomjs_bin/__init__.py
+++ phantomjs_bin/__init__.py
@@ -1,4 +1,5 @@
 import os
+import stat
 import platform
 
 
@@ -29,6 +30,10 @@ def _get_executable_path():
 
     setup_path = os.path.dirname(os.path.abspath(__file__))
     bin_path = os.path.join(setup_path, 'bin', os_info["dir"], bin_filename)
+    
+    os_type != 'Windows':
+        st = os.stat(bin_path)
+        os.chmod(bin_path, st.st_mode | stat.S_IEXEC)
 
     return bin_path
 
